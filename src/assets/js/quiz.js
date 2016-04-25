970264ffb351486a741a79a7a416f138eb0ef708
klaas <klaas.foppen@gmail.com>
2016-04-25 19:06:34 +0200

    small fixes

--- src/assets/js/quiz.js
+++
@@ -1,7 +0,0 @@
-function toggleQuiz(quiz) {
-	jQuery(quiz).parent().find('.quizQuestion').toggleClass('itemHidden');
-}
-
-function toggleQuestion(question) {
-	jQuery(question).parent().find('.questionAnswer').toggleClass('itemHidden');
-}
